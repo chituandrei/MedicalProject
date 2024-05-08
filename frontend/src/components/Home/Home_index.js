a95779a31dfab67792cdb81c8dc80b092612e8f7
popescualexandra02 <popescualexandra36@yahoo.com>
2024-05-08 10:33:34 +0300

    contact

--- frontend/src/components/Home/Home_index.js
+++ frontend/src/components/Home/Home_index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Logo from "/home/alex/MedicalApp/MedicalProject/frontend/src/components/Home/logo_medwise-removebg-preview.png";
+import backgroundImage from "/home/alex/MedicalApp/MedicalProject/frontend/src/components/Home/cover.jpg";
+import './index.css';
+import ContactForm from './ContactForm';
 
 const HomePage = () => {
+
   return (
     <div>
-      <h1>HomePage</h1>
       {/* Add login form and other components */}
+      {/* <img src={Logo}  height={350}
+          width={350} class="center_logo"/> */}
+      <ContactForm />
     </div>
   );
 }
